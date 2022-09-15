c62a5d14aafc5bacf1473e4fa1e9e427c11fd835
sargrbs <rubens.felipe@hotmail.com>
2022-09-14 22:53:30 -0300

    Add Crud

---
+++ src/Form/UserType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('status')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('privilege')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
